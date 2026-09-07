f9301fc1946d3d5e1652d9f5d58b500327d34867
colegioevolucao <roberta.peixoto@colegio-evolucao.com>
2026-09-07 19:06:05 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "COLE_AQUI_SUA_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "COLE_AQUI_SUA_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://whsxgjjkpnuwyvbqzgiy.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_e1DZJ7G8FXwfdNVp6rlEnA_Hdf_dF0b";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
